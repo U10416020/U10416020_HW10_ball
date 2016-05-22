b615501a2895ca1fca80688d87c424d35aa00487
U10416020 <wkps19961015@gmail.com>
2016-05-22 21:22:39 +0800

    Add methods
    Add addBall method to add ball.
    Add timeline method to set timeline.
    And change somethings in moveBall method to move all balls.

--- BallPane.java
+++ BallPane.java
@@ -1,5 +1,9 @@
-//package chapter15;
+//U10416020
 
+package ball;
+
+import java.security.SecureRandom;
+import java.util.ArrayList;
 import javafx.application.Application;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
@@ -10,57 +14,90 @@ import javafx.scene.shape.Circle;
 import javafx.util.Duration;
 
 public class BallPane extends Pane {
-  public final double radius = 20;
-  private double x = radius, y = radius;
-  private double dx = 1, dy = 1;
-  private Circle circle = new Circle(x, y, radius);
-  private Timeline animation;
-
-  public BallPane() {
-    circle.setFill(Color.GREEN); // Set ball color
-    getChildren().add(circle); // Place a ball into this pane
-
-    // Create an animation for moving the ball
-    animation = new Timeline(
-      new KeyFrame(Duration.millis(50), e -> moveBall()));
-    animation.setCycleCount(Timeline.INDEFINITE);
-    animation.play(); // Start animation
-  }
-
-  public void play() {
-    animation.play();
-  }
-
-  public void pause() {
-    animation.pause();
-  }
-
-  public void increaseSpeed() {
-    animation.setRate(animation.getRate() + 0.1);
-  }
-
-  public void decreaseSpeed() {
-    animation.setRate(
-      animation.getRate() > 0 ? animation.getRate() - 0.1 : 0);
-  }
-
-  public DoubleProperty rateProperty() {
-    return animation.rateProperty();
-  }
+    //DataFields
+    public final double radius = 20;  
+    private double initX = radius,initY = radius;  
+    private int count = 0;  
+    private Timeline animation;
+    private ArrayList<Circle> circle = new ArrayList<>();
+    private ArrayList<Double> initDx = new ArrayList<>();
+    private ArrayList<Double> initDy = new ArrayList<>();
+    
+    //Contructor
+    public BallPane() {
+        addBall();      
+    }
+    
+    //Add balls
+    public void addBall(){
+        circle.add(new Circle(initX, initY, radius));
+        SecureRandom random = new SecureRandom();
+        int r = random.nextInt(256);
+        int g = random.nextInt(256);
+        int b = random.nextInt(256);
+        circle.get(count).setFill(Color.rgb(r, g, b));
+        getChildren().add(circle.get(count));
+        initDx.add(1.0);
+        initDy.add(1.0);
+        timeLine(count);      
+        count++;      
+    }
   
-  protected void moveBall() {
-    // Check boundaries
-    if (x < radius || x > getWidth() - radius) {
-      dx *= -1; // Change ball move direction
+    //Set timeLine
+    public void timeLine(int currentCount){
+        animation = new Timeline(
+        new KeyFrame(Duration.millis(50), e -> moveBall(currentCount)));
+        animation.setCycleCount(Timeline.INDEFINITE);
+        animation.play(); // Start animation
     }
-    if (y < radius || y > getHeight() - radius) {
-      dy *= -1; // Change ball move direction
+    
+    //Play animation
+    public void play() {
+        animation.play();
     }
 
-    // Adjust ball position
-    x += dx;
-    y += dy;
-    circle.setCenterX(x);
-    circle.setCenterY(y);
-  }
+    //Pause animation
+    public void pause() {
+        animation.pause();
+    }
+
+    //Increase speed
+    public void increaseSpeed() {
+        animation.setRate(animation.getRate() + 0.1);
+    }
+
+    //Decrease speed
+    public void decreaseSpeed() {
+        animation.setRate(
+        animation.getRate() > 0 ? animation.getRate() - 0.1 : 0);
+    }
+
+    //Return rate property
+    public DoubleProperty rateProperty() {
+        return animation.rateProperty();
+    }
+  
+    //Set ball center
+    protected void moveBall(int currentCount) {    
+        for(int i = 0;i<=currentCount;i++){
+            double x = circle.get(i).getCenterX();
+            double y = circle.get(i).getCenterY();
+            double dx = initDx.get(i);
+            double dy = initDy.get(i);
+            if (x < radius || x > getWidth() - radius) {
+                dx *= -1; // Change ball move direction
+                initDx.set(i, dx);
+            }
+            if (y < radius || y > getHeight() - radius) {
+                dy *= -1; // Change ball move direction
+                initDy.set(i, dy);
+            }
+
+            // Adjust ball position
+            x += dx;
+            y += dy;
+            circle.get(i).setCenterX(x);
+            circle.get(i).setCenterY(y);
+        }
+    }
 }
